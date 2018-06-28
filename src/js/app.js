9889394926d4003013f7bd4f02855e8e19f870f1
kichong-windows <38249409+kichong@users.noreply.github.com>
2018-06-28 14:23:37 +0100

    corrected app.js

--- src/js/app.js
+++ src/js/app.js
@@ -24,24 +24,42 @@ App = {
   },
 
   initWeb3: function() {
-    if(typeof web3 !== 'undefined'){
-      App.web3Provider = web3.currentProvider;
-    } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    }
-    web3 = new Web3(App.web3Provider);
+     if (typeof web3 !== 'undefined') {
+         // First, we check if there's a web3 instance already active.
+         // Ethereum browsers like Mist or Chrome with the MetaMask extension
+         // will inject their own web3 instances.
+         // If an injected web3 instance is present,
+         // we get its provider and use it to create our web3 object.
+         App.web3Provider = web3.currentProvider;
+     } else {
+         // If no injected web3 instance is present,
+         // we create our web3 object based on the TestRPC's provider.
+         // Note this fallback is fine for development environments,
+         // but insecure and not suitable for production.
+         App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+     }
+     web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data){
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-      App.contracts.Adoption.setProvider(App.web3Provider);
-      return App.markAdopted();
+    $.getJSON('Adoption.json', function (data) {
+        // create contract interface using json data
+        App.contracts.Adoption = TruffleContract(data);
+
+        // set contract provider
+        App.contracts.Adoption.setProvider(App.web3Provider);
+
+        // mark adopted pet
+        return App.markAdopted();
     });
 
+    // periodically update adoption status
+    // because getAdopters() doesn't immediately return updated list of adopters
+    setInterval(App.markAdopted, 3000);
+
+    // bind events to controls
     return App.bindEvents();
   },
 
@@ -49,47 +67,58 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
-        if(adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-        }
-      }
-    }).catch(function(err) {
-      console.log(err.message);
-    });
-  },
-
-  handleAdopt: function(event) {
+  handleAdopt: function() {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
 
-    var adoptionInstance;
+    // disable button during process
+    $(this).text('Processing..').attr('disabled', true);
 
-    web3.eth.getAccounts(function(err, accounts) {
-      if(error) {
-        console.log(error);
-      }
+    // get all accounts of current user
+    web3.eth.getAccounts(function(error, accounts) {
+        if (error) {
+            console.error(error);
+        }
 
-      var account = accounts[0];
+        // get first (base) account
+        var account = accounts[0];
 
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
-
-        return adoptionInstance.adopt(petId, {from:account});
-      }).then(function(result) {
-        return App.markAdopted();
-      }).catch(function(err) {
-        console.log(err.message)
-      });
+        App.contracts.Adoption.deployed().then(function(adoptionInstance) {
+            return adoptionInstance.adopt(petId, {from: account});
+        })
+        .then(function(result) {
+            alert('Adoption success!');
+            // although it succeed, it still takes time until
+            // getAdopters() return updated list of adopters
+            return App.markAdopted();
+        })
+        .catch(function(err) {
+            // enable button again on error
+            $(this).text('Adopt').removeAttr('disabled');
+            console.log(err.message);
+        });
     });
-  
+  },
+
+  markAdopted: function(adopters, account) {
+    // get deployed contract instance
+    App.contracts.Adoption.deployed().then(function(adoptionInstance) {
+        return adoptionInstance.getAdopters.call();
+    })
+    .then(function(adopters) {
+        // disable adopted pets button
+        adopters.forEach(function(adopter, i) {
+            if (adopter !== '0x0000000000000000000000000000000000000000') {
+                $('.panel-pet').eq(i).find('button').text('Adopted').attr('disabled', true);
+            }
+        });
+        console.log('Status updated on ' + Date.now());
+    })
+    .catch(function(err) {
+        console.error(err.message);
+    });
+  }
 
 };
 
